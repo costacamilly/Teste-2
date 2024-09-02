28565d3892429ad39f128f687edc14c62a569f26
Camilly <169068284+costacamilly@users.noreply.github.com>
2024-09-02 12:14:12 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+  
+  public static void main(String[] args) {
+System.out.println("Programa que soma dois numeros");
+
+System.out.println("O primeiro número é:");
+int numero1 = 4;
+
+System.out.println(numero1);
+
+System.out.println("O segundo número é:");
+int numero2 = 9;
+
+System.out.println(numero2);
+
+System.out.println("A soma é:");
+int soma = numero1 + numero2;
+
+System.out.println(soma);
+  }
+
+
+}
